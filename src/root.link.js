82892037e6051bd3bd142103db407c846b05a01c
poommin2543 <57926799+poommin2543@users.noreply.github.com>
2022-02-10 10:56:31 +0700

    Initial commit

---
+++ src/root.link.js
@@ -0,0 +1,11 @@
+export const RESUME_PATH = 'https://drive.google.com/';
+export const GITHUB_USERNAME = 'codewithvk';
+export const LINKEDIN_USERNAME = 'vivekkumarjaviya';
+export const FIRST_NAME = 'Poommin';
+export const LAST_NAME = 'Phinphimai';
+export const ADDRESS = 'Rajkot, Gujrat'
+export const EMAIL_ID = 'poommin2543@gmail.com';
+export const TWITTER_ID = 'Vk_javiya';
+export const FRONT_PAGE_NAME = 'Poommin Phinphimai';
+export const FRONT_PAGE_DESC = 'A Full-Stack Web Developer & Open Source Contributor.I like to build things for web and solving problems.'
+
