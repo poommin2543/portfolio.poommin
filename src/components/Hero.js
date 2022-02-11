7161519093f4e4c5e368993405abb48316f57a05
poommin2543 <poommin2543@gmail.com>
2022-02-11 17:16:23 +0700

    add pic

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,7 +2,7 @@ import React, { useEffect, useRef } from 'react';
 import Lottie from 'lottie-web';
 import animate from 'lottie-web';
 import { FRONT_PAGE_DESC, FRONT_PAGE_NAME } from '../root.link';
-
+import img from './../images/picnoom.jpg'
 const Hero = () => {
   const container = useRef(null);
 
@@ -14,21 +14,32 @@ const Hero = () => {
       autoplay: true,
       animationData: require('./astronaout.json'),
     });
-    animate.setSpeed(0.5);
+    animate.setSpeed(1);
   }, []);
 
   return (
-    <section className='my-16 px-5 md:my-32 flex flex-col flex-wrap space-y-16 md:space-y-0 patterns'>
+
+    <div className='my-16 px-5 md:my-32 flex flex-col flex-wrap space-y-16 md:space-y-0 patterns'>
+        
       <div className='ml-auto w-72 md:w-96 lg:w-2/5'>
         <div className='container' ref={container}></div>
       </div>
+
+      {/* <div className = "patternsnoom">
+      <div className='ml-auto w-72 md:w-96 lg:w-2/5'>
+      <div className='container' ref={container}></div>
+      </div>  </div>
+    */}
+      
       <div className='max-w-md mb-auto space-y-5'>
         <h1 className='text-5xl font-bold md:text-7xl'>Hello. I’m {FRONT_PAGE_NAME}</h1>
         <p className='tracking-wide leading-relaxed'>
           {FRONT_PAGE_DESC}{' '}
+    <img src={img} ></img>
         </p>
       </div>
-    </section>
+      
+    </div>
   );
 };
 
